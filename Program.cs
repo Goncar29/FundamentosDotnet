a5c0b6a7f98870143ac5152a9481ef52cf536bd0
Carlos Gonzalez <51546329+Goncar29@users.noreply.github.com>
2022-06-26 14:30:56 -0300

    Update Program.cs
    Se arreglan pequeños errores

--- Program.cs
+++ Program.cs
@@ -8,7 +8,3 @@ Console.WriteLine("Por favor ingrese su edad");
 var edad = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
-
-
-/* ToWords(new System.Globalization.CultureInfo("es")) El ToWords nos sirve para que el numero que ingrese en este caso el usuario aparezca en letra, despues el new System... sirve en caso de que si tu sistema operativo lo tienes en ingles lo que hace es traducir de numero a texto pero al idioma que elijas, para este caso no era necesario pero
-se lo coloco para ejemplo */
